4fda4401135bd9ddb3f8c19a73f9ec3700dfa637
PoonYS <yeongshian.poon@timeteccloud.com>
2024-04-25 16:53:22 +0800

    final edit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import { ref } from 'vue'
 
 // Existing station in db
 const stations = ref([
-  { auto_no: uuid(), station_name: "Foyer Ambass", description: "Foyer Ambasador on 1st", staff_req: 5, enable: true },
-  { auto_no: uuid(), station_name: "Coffer Station", description: "-", staff_req: 12, enable: false },
-  { auto_no: uuid(), station_name: "Ticket Kiosk", description: "All kiosk", staff_req: 7, enable: false }
+  { auto_no: uuid(), station_name: "Foyer Ambass", description: "Foyer Ambasador on 1st", staff_req: 5, station_name_before_edit:"", description_before_edit:"", staff_req_before_edit: 0, enable: true, editing: false },
+  { auto_no: uuid(), station_name: "Coffer Station", description: "-", staff_req: 12, station_name_before_edit:"", description_before_edit:"", staff_req_before_edit: 0, enable: false, editing: false},
+  { auto_no: uuid(), station_name: "Ticket Kiosk", description: "All kiosk", staff_req: 7, station_name_before_edit:"", description_before_edit:"", staff_req_before_edit: 0, enable: false, editing: false }
 ])
 
 // New station to be added
@@ -26,8 +26,8 @@ function uuid() {
 }
 
 // Add new station to new_stations list
-const addNewStation = (station_name, description, staff_req, enable) => {
-    new_stations.value.push({auto_no: uuid(), station_name: station_name, description: description, staff_req: staff_req, enable: enable})
+const addNewStation = (station_name, description, staff_req, enable, editing) => {
+    new_stations.value.push({auto_no: uuid(), station_name: station_name, description: description, staff_req: staff_req, station_name_before_edit:"", description_before_edit:"", staff_req_before_edit: 0, enable: enable, editing: editing})
 }
 
 // Insert new station into db
@@ -55,19 +55,44 @@ function toggleCheckBox(station){
 function updateStation(station, $event, attribute){
     switch (attribute) {
     case "name":
-      station.station_name = $event.target.value;
+      station.station_name= $event.target.value;
       break;
     case "desc":
       station.description = $event.target.value;
       break;
     case "req":
-      station.staff_req = $event.target.value
+      station.staff_req = $event.target.value;
       break;
     default:
       // Error msg here
       console.log("Invalid input attribute, pick one of the following string attribute [name, desc, req]");
   }
 }
+
+// editing toggle
+function editToggle(station){
+    station.editing = !station.editing;
+}
+
+// backup filed before edit
+function backUpData(station){
+    station.station_name_before_edit = station.station_name;
+    station.description_before_edit = station.description;
+    station.staff_req_before_edit = station.staff_req;
+}
+
+// restore field to moment before edit
+function restoreData(station){
+  station.station_name = station.station_name_before_edit;
+  station.description = station.description_before_edit;
+  station.staff_req = station.staff_req_before_edit;
+}
+
+// update change
+function updateChange(station){
+    console.log("Update Change");
+}
+
 
 </script>
 
@@ -204,7 +229,7 @@ function updateStation(station, $event, attribute){
     <!-- Add new row button -->
     <div class="row justify-content-end">
       <div class="col-1">
-        <button @click="addNewStation('station', 'station description', 0, true)" type="button" class="btn btn-primary">
+        <button @click="addNewStation('station', 'station description', 0, true, false)" type="button" class="btn btn-primary">
           <i class="bi bi-plus"></i>
         </button>
       </div>
@@ -221,7 +246,7 @@ function updateStation(station, $event, attribute){
             <input :value="station.description" @input="updateStation(station, $event, 'desc')" class="form-control">
           </td>
           <td>
-            <input :value="station.staff_req" @input="updateStation(station, $event, 'req')" class="form-control">
+            <input :value="station.staff_req" @input="updateStation(station, $event, 'req')" class="form-control" type="number" min="0">
           </td>
           <td>
             <!-- Toggle check box -->
@@ -249,27 +274,43 @@ function updateStation(station, $event, attribute){
     <table class="table">
       <tbody>
         <tr v-for="(station, index) in stations" :key="station.auto_no">
-          <td>{{ station.station_name }}</td>
-          <td>{{ station.description }}</td>
-          <td>{{ station.staff_req }}</td>
           <td>
+            <input :value="station.station_name" @input="updateStation(station, $event, 'name')" class="form-control" :class="{'form-control-plaintext': !station.editing}" :readonly="!station.editing">
+          </td>
+          <td>
+            <input :value="station.description" @input="updateStation(station, $event, 'desc')" class="form-control" :class="{'form-control-plaintext': !station.editing}" :readonly="!station.editing">
+          </td>
+          <td>
+            <input :value="station.staff_req" @input="updateStation(station, $event, 'req')" class="form-control" :class="{'form-control-plaintext': !station.editing}" :readonly="!station.editing" type="number" min="0">
+          </td>
+          <td>
+            <!-- Toggle button -->
             <div class="form-check form-switch">
               <input class="form-check-input" type="checkbox" role="switch" :checked="station.enable">
             </div>
           </td>
           <td>
-            <button type="button" class="btn btn-primary">
+            <!-- Edit button -->
+            <button @click="editToggle(station); backUpData(station)" type="button" class="btn btn-primary" v-if="!station.editing">
               <i class="bi bi-pen"></i>
+            </button>
+            <!-- Confirm edit change button -->
+            <button @click="editToggle(station); updateChange(station)" type="button" class="btn btn-success" v-else>
+              <i class="bi bi-check"></i>
             </button>
           </td>
           <td>
-            <button @click="deleteStationFromDB(station)" type="button" class="btn btn-primary">
+            <!-- Delete button -->
+            <button @click="deleteStationFromDB(station)" type="button" class="btn btn-primary" v-if="!station.editing">
               <i class="bi bi-trash"></i>
+            </button>
+             <!-- Cancel edit change button -->
+            <button @click="editToggle(station); restoreData(station)" type="button" class="btn btn-danger" v-else>
+              <i class="bi bi-x"></i>
             </button>
           </td>
         </tr>
       </tbody>
     </table>
   </div>
-
 </template>
