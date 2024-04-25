17902f39af68cc9d1756b088d94cc4d37cf63c74
PoonYS <yeongshian.poon@timeteccloud.com>
2024-04-25 17:34:30 +0800

    remove redundant css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 
-// Existing station in db
+// Existing stations in db
 const stations = ref([
   { auto_no: uuid(), station_name: "Foyer Ambass", description: "Foyer Ambasador on 1st", staff_req: 5, station_name_before_edit:"", description_before_edit:"", staff_req_before_edit: 0, enable: true, editing: false },
   { auto_no: uuid(), station_name: "Coffer Station", description: "-", staff_req: 12, station_name_before_edit:"", description_before_edit:"", staff_req_before_edit: 0, enable: false, editing: false},
@@ -97,131 +97,6 @@ function updateChange(station){
 </script>
 
 <style>
-.label-wrapper {
-  margin: 0;
-  flex: 0 0 100%;
-  text-align: center;
-}
-
-[class*="__lg"] {
-  display: inline-block;
-  width: 100%;
-  font-size: 1.9rem;
-}
-
-[class*="__lg"]:not(:last-child) {
-  margin-bottom: 1rem;
-}
-
-@media screen and (min-width: 620px) {
-  [class*="__lg"] {
-    font-size: 2.4rem;
-  }
-}
-
-.visually-hidden {
-  position: absolute;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(1px 1px 1px 1px);
-  clip: rect(1px, 1px, 1px, 1px);
-  clip-path: rect(1px, 1px, 1px, 1px);
-  white-space: nowrap;
-}
-
-[class*="stack"]>* {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-
-.stack-small>*+* {
-  margin-top: 1.25rem;
-}
-
-.stack-large>*+* {
-  margin-top: 2.5rem;
-}
-
-@media screen and (min-width: 550px) {
-  .stack-small>*+* {
-    margin-top: 1.4rem;
-  }
-
-  .stack-large>*+* {
-    margin-top: 2.8rem;
-  }
-}
-
-/* End global styles */
-#app {
-  background: #fff;
-  margin: 2rem 0 4rem 0;
-  padding: 1rem;
-  padding-top: 0;
-  position: relative;
-  box-shadow:
-    0 2px 4px 0 rgb(0 0 0 / 20%),
-    0 2.5rem 5rem 0 rgb(0 0 0 / 10%);
-}
-
-@media screen and (min-width: 550px) {
-  #app {
-    padding: 4rem;
-  }
-}
-
-#app>* {
-  max-width: 50rem;
-  margin-left: auto;
-  margin-right: auto;
-  font-size: 10pt;
-}
-
-#app>form {
-  max-width: 100%;
-}
-
-#app h1 {
-  display: block;
-  min-width: 100%;
-  width: 100%;
-  text-align: center;
-  margin: 0;
-  margin-bottom: 1rem;
-}
-
-
-
-/* table css */
-.table {
-  width: 100%;
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-
-.table th,
-.table td {
-  border: 1px solid #dee2e6;
-  padding: 8px;
-}
-
-.table th {
-  background-color: #f8f9fa;
-  text-align: left;
-}
-
-/* Style the form switch */
-.form-switch {
-  display: inline-flex;
-  align-items: center;
-}
-
-.form-switch input[type="checkbox"] {
-  margin-right: 5px;
-}
-
-
 </style>
 
 <template>
